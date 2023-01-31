38fc2bebb388e874c93f81c036f4df733a78b962
Alexandre Marra <alexsmarra@hotmail.com>
2023-01-31 19:26:53 -0300

    migrations tags and links created

---
+++ src/database/knex/migrations/20230131220436_createLinks.js
@@ -0,0 +1,10 @@
+// to create
+exports.up = knex => knex.schema.createTable("links", table => {
+   table.increments("id")
+   table.text("url").notNullable()
+   table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE")
+   table.timestamp("created_at").default(knex.fn.now())
+})
+
+// to delete
+exports.down = knex => knex.schema.dropTable("links")
