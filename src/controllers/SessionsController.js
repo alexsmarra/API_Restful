b43fcf405f3793b3f12cded6ee81b4e2032b99ed
Alexandre Marra <alexsmarra@hotmail.com>
2023-04-06 11:00:18 -0300

    validating password

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
 const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
+const { compare } = require("bcryptjs")
 
 class SessionsController {
    async create(req, res) {
@@ -8,7 +9,14 @@ class SessionsController {
       const user = await knex("users").where({ email }).first()
 
       if(!user) {
-         throw new AppError("Wrong e-mail or password", 401)
+         throw new AppError("Wrong email or password", 401)
+      }
+
+      const passwordMatched = await compare(password, user.password)
+
+      // we will not specify if the password or email is incorrect, so as not to make it easier for someone who wants to find out the data of a user, some thief
+      if(!passwordMatched) {
+         throw new AppError("Wrong email or password", 401)
       }
 
       return res.json(user)
