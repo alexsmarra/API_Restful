be4b3fa7820dd7714f6190b5fae5e9f10d6afbde
Alexandre Marra <alexsmarra@hotmail.com>
2023-04-11 12:36:17 -0300

    saving image in database

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,6 +3,7 @@ const multer = require("multer")
 const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/UsersController")
+const UserAvatarController = require("../controllers/UserAvatarController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const usersRoutes = Router()
@@ -10,13 +11,11 @@ const usersRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
+const usersAvatarController = new UserAvatarController()
 
 usersRoutes.post("/", usersController.create)
 usersRoutes.put("/", ensureAuthenticated, usersController.update)
 // the 'put' method is when we want to update several fields, while the 'patch' method is when we want to update only one field, which in this case is the user's avatar. "upload.single()" is because we want upload only one file. 
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
-   console.log(req.file.filename)
-   res.json()
-})
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), usersAvatarController.update)
 
 module.exports = usersRoutes
